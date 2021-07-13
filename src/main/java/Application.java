a5254cbcb9f3d458b759edca51b47161b9bffb84
tranvutruong <truongtv1399it@gmail.com>
2021-07-13 11:51:34 +0700

    Update main in project show size

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,13 @@
+import entity.DataProvinceInVietNam;
 import service.GetAllDataOfJSON;
 import service.impl.GetAllDataOfJSONImpl;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         GetAllDataOfJSON iam = new GetAllDataOfJSONImpl();
-        iam.getAll();
-        System.out.println("Hello");
+        List<DataProvinceInVietNam> data = iam.getAll();
+        System.out.println("Size data "+data.size());
     }
 }
