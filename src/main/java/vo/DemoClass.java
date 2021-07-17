de1d66aebe6608ee9b60beee7f905a9117e0d989
tranvutruong <truongtv1399it@gmail.com>
2021-07-17 11:36:11 +0700

    update file json

--- src/main/java/vo/DemoClass.java
+++ src/main/java/vo/DemoClass.java
@@ -1,6 +1,22 @@
 package vo;
 
 public class DemoClass {
-    private int id;
     private String name;
+    private Double tbPopulation;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getTbPopulation() {
+        return tbPopulation;
+    }
+
+    public void setTbPopulation(Double tbPopulation) {
+        this.tbPopulation = tbPopulation;
+    }
 }
